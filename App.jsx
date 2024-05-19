01350e5375a0d5577c2103a30ef0db9890e70960
Yashika-1705 <yashigcp17@gmail.com>
2024-05-19 15:47:15 -0400

    Update App.jsx to handle deleting task and creating local storage for tasks

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 import "./App.css"
 import TaskForm from './components/TaskForm'
@@ -7,17 +7,48 @@ import todoIcon from './assets/direct-hit.png'
 import doingIcon from './assets/glowing-star.png'
 import doneIcon from './assets/check-mark-button.png'
 
+const oldtasks = localStorage.getItem("tasks")
+console.log(oldtasks)
+
+
+
 
 const App = () => {
-  const [tasks, settasks] = useState([])
-  console.log("tasks", tasks)
+  const [tasks, settasks] = useState(JSON.parse(oldtasks) || [])
+
+  useEffect(() => {
+      localStorage.setItem("tasks", JSON.stringify(tasks))
+  },[tasks])
+
+  const handleDelete = (taskIndex) => {
+    const newtasks = tasks.filter((task, index) => index !== taskIndex)
+    settasks(newtasks)
+  }
   return (
     <div className='app'>
       <TaskForm settasks={settasks}/>
       <main className='app_main'>
-        <TaskColumn title = "To Do" icon = {todoIcon} tasks={tasks} status = "todo"/>
-        <TaskColumn title = "Doing" icon = {doingIcon} tasks={tasks} status = "doing"/>
-        <TaskColumn title = "Done" icon = {doneIcon} tasks={tasks} status = "done"/>
+        <TaskColumn 
+          title = "To Do" 
+          icon = {todoIcon} 
+          tasks={tasks} 
+          status = "todo"
+          handleDelete={handleDelete}
+        />
+        <TaskColumn 
+          title = "Doing" 
+          icon = {doingIcon} 
+          tasks={tasks} 
+          status = "doing"
+          handleDelete={handleDelete}
+        />
+        <TaskColumn 
+          title = "Done" 
+          icon = {doneIcon} 
+          tasks={tasks} 
+          status = "done"
+          handleDelete={handleDelete}
+        />
       </main>
     </div>
   )
